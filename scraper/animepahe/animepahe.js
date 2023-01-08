2c141c22666c4359ff9a3e3ab6b91b1d46083fff
IGRohan <volumezero2508@gmail.com>
2023-01-08 16:23:33 +0530

    animepahe headers

--- scraper/animepahe/animepahe.js
+++ scraper/animepahe/animepahe.js
@@ -3,6 +3,7 @@ import { load } from 'cheerio';
 
 const animepaheBase = `https://animepahe.com/`;
 const animepaheApi = `https://animepahe.com/api`;
+const USER_AGENT = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36`;
 import { extractSource } from '../../helper/kwik.js';
 
 
@@ -17,6 +18,9 @@ export const fetchSearchAnimepahe = async ({ keyw, list = [] }) => {
             params: {
                 m: 'search',
                 q: keyw
+            },
+            headers: {
+                "User-Agent": USER_AGENT
             }
         });
 
